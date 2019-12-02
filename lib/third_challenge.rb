c38cc8ce4fe056eefc0794e56ddd6edf4d39531d
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-02 07:26:55 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,11 +2,18 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague"
+        :name => "Lord Montague",
         :age => "53"
       },
-      :matriarch => {},
-      :hero => {},
+      :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
+      },
+      :hero => {
+        :name => "Lord Montague"
+        :age => "15"
+        :status => "alive"
+      },
       :hero_friends => []
    }, 
    :capulet => {
